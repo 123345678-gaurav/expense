6652a9d54c3d8a6880970519c3b1b7759ce7d448
Gourav Agarwal <gauravagarwa2028@gmail.com>
2021-06-01 13:02:28 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Expenseitem from "./components/Expenseitem";
+import NewexpenseItem from "./components/NewexpenseItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = ()=>
+{
+  const ExpenseitemList = [
+    {
+      id: 1,
+      date: "28 March 2021",
+      title: "Toilet Paper",
+      amount: "237.12"
+    },
+    {
+      id: 1,
+      date: "28 April 2021",
+      title: "House Rent",
+      amount: "127.12"
+    },
+    {
+      id: 1,
+      date: "28 May 2021",
+      title: "Car Insurance",
+      amount: '627.12'
+    }
+  ]
+  return<>
+  <NewexpenseItem />
+  <Expenseitem 
+    title={ExpenseitemList[0].title}
+    amount={ExpenseitemList[0].amount}
+    date={ExpenseitemList[0].date}
+  />
+  <Expenseitem 
+    title={ExpenseitemList[1].title}
+    amount={ExpenseitemList[1].amount}
+    date={ExpenseitemList[1].date}
+  />
+  <Expenseitem 
+    title={ExpenseitemList[2].title}
+    amount={ExpenseitemList[2].amount}
+    date={ExpenseitemList[2].date}
+  />
+  </>
 }
 
 export default App;
